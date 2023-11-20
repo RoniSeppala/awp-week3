4d5eb080f84109b5a76a5cfdd28fe373c4676049
Roni Seppälä <roni.o.seppala@gmail.com>
2023-11-20 20:37:46 +0200

    made feature 3

--- AWPweek3/public/javascripts/script.js
+++ AWPweek3/public/javascripts/script.js
@@ -1,8 +1,11 @@
-
 const sumbitButton = document.getElementById("submit-data");
 const inputName = document.getElementById("input-name");
 const inputTask = document.getElementById("input-task");
 const textOutput = document.getElementById("output");
+
+const searchButton = document.getElementById("search")
+const searchField = document.getElementById("search-name")
+const resutbox = document.getElementById("returnField")
 
 sumbitButton.addEventListener('click', () => {
     const name = inputName.value;
@@ -16,7 +19,44 @@ sumbitButton.addEventListener('click', () => {
         body:JSON.stringify( {name, todo: task} )
     }).then(response => response.text())
     .then(data => {
-        console.log(data);
         textOutput.innerText = data;
+    })
+})
+
+searchButton.addEventListener('click', () => {
+    const searchName = searchField.value;
+
+    fetch("http://localhost:3000/user/" + searchName).then(response => response.json())
+    .then(data => {
+        console.log(data)
+
+        resutbox.innerHTML = ""
+
+        if (data.response == "No user searched"){
+            let responseElement = document.createElement("h3")
+            responseElement.innerText = "No user searched"
+            resutbox.appendChild(responseElement)
+        } else if (data.response == "Data not found"){
+            let responseElement = document.createElement("h3")
+            responseElement.innerText = "Data not found"
+            resutbox.appendChild(responseElement)
+        } else if (data.response == "Data found"){
+            let responseElement = document.createElement("h3")
+            responseElement.innerText = data.data.name
+            resutbox.appendChild(responseElement)
+
+            let todoList = document.createElement("ul")
+            data.data.todos.forEach(element => {
+                let todo = document.createElement("li")
+                todo.innerText = element;
+                todoList.appendChild(todo);
+            });
+            resutbox.appendChild(todoList)
+        } else {
+            let responseElement = document.createElement("h3")
+            responseElement.innerText = "Error fetching data"
+            resutbox.appendChild(responseElement)
+
+        }
     })
 })
